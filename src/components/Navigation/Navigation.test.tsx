95f9fca294e14d95ee8d015a1722eef02012dd97
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-06-01 21:02:49 +0200

    tested uncovered lines in Navigation test

--- src/components/Navigation/Navigation.test.tsx
+++ src/components/Navigation/Navigation.test.tsx
@@ -1,8 +1,16 @@
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "../../redux/store/store";
 import Navigation from "./Navigation";
+
+const mockDispatch = jest.fn();
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: () => mockDispatch,
+}));
 
 describe("Given a Navbar component function", () => {
   describe("When invoked", () => {
@@ -20,6 +28,23 @@ describe("Given a Navbar component function", () => {
 
       expect(navbar).toBeInTheDocument();
       expect(logoutButton).toBeInTheDocument();
+    });
+  });
+
+  describe("When invoked and the user clicks on the logout button", () => {
+    test("Then it should invoke the logout function", () => {
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <Navigation />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const logoutButton = screen.getByRole("button", { name: "Logout" });
+      userEvent.click(logoutButton);
+
+      expect(mockDispatch).toHaveBeenCalled();
     });
   });
 });
--- src/pages/AccessFormPage.test.tsx
+++ src/pages/AccessFormPage.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "../redux/store/store";
-import AccessFormPage from "./AccesFormPage";
+import AccessFormPage from "./AccessFormPage";
 
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
