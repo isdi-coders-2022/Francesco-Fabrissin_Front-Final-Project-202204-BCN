765d19b050331365cbb684a136e9084b5cb8de3e
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-06-04 17:58:11 +0200

    tested RecordsList component

---
+++ src/components/RecordsList/RecordsList.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { mockRecords } from "../../mocks/mockRecords";
+import store from "../../redux/store/store";
+import RecordsList from "./RecordsList";
+
+describe("Given a RecordsList component function", () => {
+  describe("When invoked with 2 records", () => {
+    test("Then it should render 2 list elements", () => {
+      const expectedNumberOfRecords = 2;
+      const records = mockRecords;
+
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <RecordsList records={records} />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const listElements = screen.getAllByRole("listitem");
+      expect(listElements).toHaveLength(expectedNumberOfRecords);
+    });
+  });
+});
