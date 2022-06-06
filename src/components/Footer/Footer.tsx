85a0aacae160ed00b97f7fac2a59d88c299726fb
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-06-06 12:51:01 +0200

    created Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import FooterStlyed from "./FooterStyled";
+
+const Footer = () => {
+  return (
+    <FooterStlyed>
+      <img
+        src="/images/RecordSwapp-logo.png"
+        alt="RecordSwapp logo"
+        className="logo"
+      />
+
+      <span>© 2022 RecordSwapp®</span>
+    </FooterStlyed>
+  );
+};
+
+export default Footer;
---
+++ src/components/Footer/FooterStyled.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const FooterStlyed = styled.div`
+  width: 100%;
+  height: 5rem;
+  background-color: #dae1e9;
+  display: flex;
+  align-items: center;
+
+  .logo {
+    width: 7rem;
+  }
+
+  span {
+    position: absolute;
+    left: 50%;
+    width: 13rem;
+    transform: translate(-6.5rem);
+    font-weight: 600;
+    font-size: 1.2rem;
+    justify-self: center;
+  }
+`;
+
+export default FooterStlyed;
