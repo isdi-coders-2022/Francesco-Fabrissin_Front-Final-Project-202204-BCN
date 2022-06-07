c61ab568cd0fb40001961c51aacdcaafb317e12d
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-06-07 16:33:19 +0200

    corrected initial state in recordReducer test

--- src/redux/features/recordSlice.test.ts
+++ src/redux/features/recordSlice.test.ts
@@ -1,10 +1,22 @@
 import { mockRecords } from "../../mocks/mockRecords";
+import { IRecord } from "../../types/types";
 import recordSlice, { loadRecordActionCreator } from "./recordSlice";
 
 describe("Given a recordReducer", () => {
   describe("When it receives an initial empty state and a loadRecords action with the new record info", () => {
     test("Then it should return a new record state with the new record info", () => {
-      const initialStatus: IRecord = {};
+      const initialStatus: IRecord = {
+        id: "",
+        title: "",
+        artist: "",
+        year: "",
+        genre: "",
+        conditions: "",
+        image: "",
+        price: "",
+        owner: "",
+        youtube_url: "",
+      };
       const recordPayload = mockRecords[0];
 
       const expectedNewState = { ...recordPayload };
