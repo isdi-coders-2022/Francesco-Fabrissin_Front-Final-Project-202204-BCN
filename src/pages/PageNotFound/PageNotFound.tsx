a128251134ca09338c3b64becfb04edff99314f7
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-06-13 13:24:16 +0200

    created PageNotFound + route

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,32 +1,27 @@
-import styled from "styled-components";
-
-const PageNotFoundStyled = styled.div`
-  position: fixed;
-  z-index: 10;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  background-color: rgba(250, 250, 250, 1);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  .number {
-    display: flex;
-  }
-`;
+import PageNotFoundStyled from "./PageNotFoundStyled";
+import { AiFillHome } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 const PageNotFound = () => {
+  const navigate = useNavigate();
+
+  const goToHome = () => {
+    navigate("/users/collections");
+  };
+
   return (
     <PageNotFoundStyled>
-      <div className="number">
+      <div className="home-button">
+        <AiFillHome data-testid="icon-home" onClick={goToHome} size={35} />
+        <span>Back to Home</span>
+      </div>
+      <div className="number-404">
         <span className="number">4</span>
         <img src="/images/record-icon.png" alt="" className="record-icon" />
         <span className="number">4</span>
       </div>
-      <span>Sorry, page not found</span>
+      <span className="message">Sorry,</span>
+      <span className="message">page not found</span>
     </PageNotFoundStyled>
   );
 };
